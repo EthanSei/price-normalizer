f2d7c01e10b1f448b7f8e638a785a49327cbdf43
Ethan Seide <eseide@princeton.edu>
2021-02-13 15:56:02 -0500

    Initial template and starting arcgis

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from flask import Flask, request, make_response, redirect, url_for
 from flask import render_template, session, flash
+from arcgis.gis import GIS
 
 #-----------------------------------------------------------------------
 
 app = Flask(__name__)
+gis = GIS()
 
 @app.route('/', methods=['GET'])
 def index():
-    return "hello world"
+    html = render_template('home.html', session=session)
+    return make_response(html)
 
 @app.route('/profile', methods=['GET'])
 def profile():
     return "profile"
+    
 
 if __name__ == '__main__':
     app.run('localhost', port=55555, debug=True)
